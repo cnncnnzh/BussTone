bb37f0deb236c52290922892279fdd3e211e0913
cnncnnzh@gmail.com <cnncnnzh@gmail.com>
2023-12-02 00:27:02 -0600

    update extract method

--- BussTone/get_score.py
+++ BussTone/get_score.py
@@ -29,60 +29,60 @@ class Score():
     def gen_score(self, model, tokenizer, split_sentence):
         all_files = sorted(os.listdir(self.root))
         log_file = os.path.join(self.result_root, 'log.txt')
-        for kid in all_files[0:10]:
-            # try:
-            path1 = os.path.join(os.path.join(self.root, kid), r'10-K')
-            for kid_v in os.listdir(path1):
-                print('analyzing {} ...'.format(kid_v))
-                prekid, date = kid_v.split('_')
-                # kid, year, _ = prekid.split('-')
-                root = os.path.join(path1, kid_v)
-                dirc = os.path.join(root, 'full-submission.txt')
-                to_dirc = os.path.join(root, 'script.txt')
-                
-                #convert html to txt
-                if self.to_txt == 'true':
-                    html_to_txt(dirc, to_dirc)
-                elif os.path.exists(to_dirc):
-                    print('{} existed'.format(to_dirc))
-                else:
-                    Exception('Error: {} not found'.format(to_dirc)) 
+        for kid in all_files[0:100]:
+            try:
+                path1 = os.path.join(os.path.join(self.root, kid), r'10-K')
+                for kid_v in os.listdir(path1):
+                    print('analyzing {} ...'.format(kid_v))
+                    prekid, date = kid_v.split('_')
+                    # kid, year, _ = prekid.split('-')
+                    root = os.path.join(path1, kid_v)
+                    dirc = os.path.join(root, 'full-submission.txt')
+                    to_dirc = os.path.join(root, 'script.txt')
                     
-                #extract all the paragraphs
-                all_scripts = get_paragraph(to_dirc, split_sentence)
-                
-                # save extracted paragraphs
-                
-                to_file = os.path.join(self.result_root, kid + '-' + date + '.txt')
-
-                # generate tone scores
-                for i, script in enumerate(all_scripts):
-                    if len(script) < 35:
+                    #convert html to txt
+                    if self.to_txt == 'true':
+                        html_to_txt(dirc, to_dirc)
+                    elif os.path.exists(to_dirc):
+                        print('{} existed'.format(to_dirc))
+                    else:
+                        Exception('Error: {} not found'.format(to_dirc)) 
+                        
+                    #extract all the paragraphs
+                    all_scripts = get_paragraph(to_dirc, split_sentence)
+                    
+                    # save extracted paragraphs
+                    
+                    to_file = os.path.join(self.result_root, kid + '-' + date + '.txt')
+    
+                    # generate tone scores
+                    for i, script in enumerate(all_scripts):
+                        if len(script) < 35:
+                            if self.write_script == 'true':
+                                gen_script(script, to_file)
+                            continue         
+                        inputs = tokenizer(script, return_tensors="pt")   
+                        with torch.no_grad():
+                            logits = model(**inputs).logits.squeeze().tolist()
+                        self.table = pd.concat([self.table, pd.DataFrame(
+                            {
+                                'KID':[kid],
+                                'Date':[date],
+                                'Label':[i],
+                                'Positive':[logits[0]],
+                                'Negative':[logits[1]],
+                                'Neutral':[logits[2]]
+                            }
+                        )], ignore_index=True)
+                        # print(self.table)
                         if self.write_script == 'true':
                             gen_script(script, to_file)
-                        continue         
-                    inputs = tokenizer(script, return_tensors="pt")   
-                    with torch.no_grad():
-                        logits = model(**inputs).logits.squeeze().tolist()
-                    self.table = pd.concat([self.table, pd.DataFrame(
-                        {
-                            'KID':[kid],
-                            'Date':[date],
-                            'Label':[i],
-                            'Positive':[logits[0]],
-                            'Negative':[logits[1]],
-                            'Neutral':[logits[2]]
-                        }
-                    )], ignore_index=True)
-                    # print(self.table)
-                    if self.write_script == 'true':
-                        gen_script(script, to_file)
-                        gen_script(str(logits), to_file)
-            # except:
-            #     print('Analyzing {} not successful'.format(dirc))
-            #     with open (log_file, 'a', encoding='utf-8') as f:
-            #         f.write('Analyzing {} not successful'.format(dirc))
-            #         f.write('\n\n')
+                            gen_script(str(logits), to_file)
+            except:
+                print('Analyzing {} not successful'.format(dirc))
+                with open (log_file, 'a', encoding='utf-8') as f:
+                    f.write('Analyzing {} not successful'.format(dirc))
+                    f.write('\n\n')
                   
         
     def write_score(self):
